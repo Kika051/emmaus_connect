7d91c0d8e39eeae071dc25c123a9f426ee2f4316
christophe Dubois <dubois51ch@gmail.com>
2023-06-28 13:52:00 +0200

    test + nettoyage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import Home from "./pages/Home";
+import Main from "./pages/Main";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Main />
     </div>
   );
 }
